a19a92f370a479276a72c79e78cb0f31ca670120
Michal Koperski <mkoperpno@gmail.com>
2020-10-26 20:55:23 +0100

    show all Companies in website

--- src/main/java/com/example/demo/controller/FrontendController.java
+++ src/main/java/com/example/demo/controller/FrontendController.java
@@ -1,10 +1,19 @@
 package com.example.demo.controller;
 
+import com.example.demo.model.Company;
+import com.example.demo.service.CompanyService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
 
 @Controller
 public class FrontendController {
+
+    @Autowired
+    private CompanyService companyService;
 
     @GetMapping("/about")
     public String about() {
@@ -12,7 +21,9 @@ public class FrontendController {
     }
 
     @GetMapping("/")
-    public String indext() {
+    public String viewHomePage(Model model) {
+        List<Company> companyList = companyService.findAllCompany();
+        model.addAttribute("listComapnies", companyList);
         return "index";
     }
 }
